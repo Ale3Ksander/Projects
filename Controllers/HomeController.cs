5046e8df89e07209f20609aeb863cb63baaf86e3
Ale3Ksander <ksander.l27@gmail.com>
2020-03-23 19:11:53 +0200

    message

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using NinjaCore.Models;
+
+namespace NinjaCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private UserContext db;
+        public HomeController(UserContext context)
+        {
+            db = context;
+        }
+
+        public IActionResult Logo()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View(db.Users.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(User user)
+        {         
+            db.Users.Add(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {            
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }            
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(User user)
+        {
+            db.Users.Update(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet, ActionName("Delete")]
+        public IActionResult ConfirmDelete(int id)
+        {
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            //User user = db.Users.FirstOrDefault(u => u.Id == id);
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
+            //db.Entry(user).State = EntityState.Deleted;
+            db.SaveChanges();
+            return RedirectToAction("Index");       
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NinjaCore.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required (ErrorMessage = "FirstName not specified")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "LastName not specified")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email not specified"), EmailAddress]
+        public string Email { get; set; }
+
+        public DateTime BirthDate { get; set; }
+    }
+}
